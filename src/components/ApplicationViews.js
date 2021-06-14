7d2fb6de37284b48c82fa8a37da3f3c4cdf8ed45
sllittrell <stacey.littrell@gmail.com>
2021-06-14 10:55:06 -0500

    react app setup, login/logout, and registration complete

---
+++ src/components/ApplicationViews.js
@@ -0,0 +1,24 @@
+import React from "react"
+import { Route } from "react-router-dom"
+import { Profile } from "./Profile.js"
+import { ProfileProvider } from "./auth/ProfileProvider.js"
+import { Home } from "./Home.js"
+
+export const ApplicationViews = () => {
+    return <>
+        <main style={{
+            margin: "5rem 2rem",
+            lineHeight: "1.75rem"
+        }}>
+            
+            <Route exact path="/">
+                <Home/>
+            </Route>
+    
+            {/* ______________posts__________________________ */}
+
+            {/* _________________Profile______________________________ */}
+               
+        </main>
+    </>
+}
