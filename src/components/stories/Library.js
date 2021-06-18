f913f17dc84627a950ffd1965833ffdf08d2b406
sllittrell <stacey.littrell@gmail.com>
2021-06-18 16:20:56 -0500

    Can create visual schedule and view in users library

--- src/components/stories/Library.js
+++ src/components/stories/Library.js
@@ -1,16 +1,19 @@
 import React, { useContext, useEffect, useState } from 'react'
-import { Button, Card } from 'react-bootstrap'
+import { Button, Card, Col, Row } from 'react-bootstrap'
 import { useHistory } from 'react-router'
+import { ScheduleContext } from '../schedules/ScheduleProvider'
 import {StoryContext } from './StoryProvider'
 
 export const Library = () => {
     const {getStories, stories} =useContext(StoryContext)
+    const {getSchedules, schedules}=useContext(ScheduleContext)
     const userId = parseInt(localStorage.getItem(`lu_token`))
     const history =useHistory()
     const [filterStory, setFilterStory] =useState()
 
     useEffect(() =>{
         getStories()
+        .then(()=>getSchedules())
     },[])
 
     useEffect(()=>{
@@ -21,16 +24,28 @@ export const Library = () => {
     return (
         <>
             <h2>My Social stories</h2>
-            <div>
+            <Row>
+                <Col>
                 {stories.map((story)=> filterStory ? 
                 <Card key ={story.id} style={{ width: '18rem' }}>
                 <Card.Img variant="top" src={story.title_image} />
                 <Card.Body>
                   <Card.Title>{story.titlepage}</Card.Title>
                   <Button variant="primary" onClick={()=> history.push(`./story/${story.id}`)}>View Story</Button>
-                </Card.Body>
-              </Card> : <></>)}
-            </div>
+                </Card.Body>        
+              </Card> : <></>)}</Col>
+              <Col>
+              <h2>My Visual Schedules</h2>
+              {schedules.map((sched)=>
+              <Card key ={sched.id} style={{ width: '18rem' }}>
+              <Card.Img variant="top" src={sched.image_1} />
+              <Card.Body>
+                <Card.Title>{sched.title}</Card.Title>
+                <Button variant="primary" onClick={()=> history.push(`./story/${sched.id}`)}>View Story</Button>
+              </Card.Body>        
+            </Card>
+              )}</Col>
+            </Row>
         </>
     )
 }
